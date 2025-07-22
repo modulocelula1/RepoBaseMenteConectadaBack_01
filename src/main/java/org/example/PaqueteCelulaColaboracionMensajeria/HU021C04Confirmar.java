22007a5d940080ee9e1a5c9f84ee14ccc80a005f
Dariannys   Daniela Zambrano  Escobar <dariannys.zambrano@globant.com>
2025-07-22 11:11:35 -0500

    feature

--- src/main/java/org/example/PaqueteCelulaColaboracionMensajeria/HU021C04Confirmar.java
+++ src/main/java/org/example/PaqueteCelulaColaboracionMensajeria/HU021C04Confirmar.java
@@ -1,4 +1,7 @@
 package org.example.PaqueteCelulaColaboracionMensajeria;
 
 public class HU021C04Confirmar {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
