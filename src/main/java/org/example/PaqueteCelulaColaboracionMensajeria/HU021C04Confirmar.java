8891962ac02bd2ec4fff7e20ed15f3a859bdc77e
Dariannys   Daniela Zambrano  Escobar <dariannys.zambrano@globant.com>
2025-07-25 10:11:44 -0500

    Refactorizacion del codigo

--- src/main/java/org/example/PaqueteCelulaColaboracionMensajeria/HU021C04Confirmar.java
+++ src/main/java/org/example/PaqueteCelulaColaboracionMensajeria/HU021C04Confirmar.java
@@ -4,23 +4,18 @@ import java.util.Scanner;
 
 public class HU021C04Confirmar {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         String nombreUsuario;
         String correoUsuario;
-
-        System.out.println("\n Por favor ingrese su nombre completo ");
-        nombreUsuario= scanner.nextLine();
-
-        System.out.println("\n por favor ingrese su correo electronico ");
-        correoUsuario= scanner.nextLine();
-
-        if (nombreUsuario.equals("") || correoUsuario.equals("")){
-            System.out.println("Error, validar que los campos no esten vacioss ");
-
-        } else {
-            System.out.println("felicidades " + nombreUsuario + " se ha registrado correctamente. ");
-
-        }
+        do {
+            System.out.println("Por favor ingrese su nombre completo: ");
+            nombreUsuario = scanner.nextLine();
+        } while (nombreUsuario.trim().isEmpty());
+        do {
+            System.out.println("Por favor ingrese su correo electrónico: ");
+            correoUsuario = scanner.nextLine();
+        } while (correoUsuario.trim().isEmpty());
+        System.out.println("felicidades! " + nombreUsuario + " se ha registrado correctamente ");
     }
 }
